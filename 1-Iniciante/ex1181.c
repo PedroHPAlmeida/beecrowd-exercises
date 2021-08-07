d9c053cf65a62a84cddc4f4d53fc4a52669bbc1d
Pedro Henrique Pereira Almeida <pedro.6571almeida@gmail.com>
2021-08-07 17:01:41 -0300

    Linha na Matriz

---
+++ 1-Iniciante/ex1181.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define ORD 12
+
+int main(){
+    /*variaveis*/
+    int l, i, j;
+    char t;
+    double m[ORD][ORD], soma;
+
+    /*entrada*/
+    scanf("%d", &l);
+    scanf(" %c", &t);
+    for(i = 0; i < ORD; i++){
+        for(j = 0; j < ORD; j++){
+            scanf("%lf", &m[i][j]);
+        }
+    }
+
+    /*processamento*/
+    for(j = 0; j < ORD; j++){
+        soma += m[l][j];
+    }
+
+    /*saida*/
+    if(t == 'S'){
+        printf("%.1lf\n", soma);
+    }
+    else{
+        printf("%.1lf\n", soma / (float)ORD);
+    }
+
+    return 0;
+}
