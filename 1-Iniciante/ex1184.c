4504a40c52ad19d231a7a8c16a30a169efee7020
Pedro Henrique Pereira Almeida <pedro.6571almeida@gmail.com>
2021-08-08 20:59:59 -0300

    Abaixo da Diagonal Principal

---
+++ 1-Iniciante/ex1184.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define ORD 12
+
+int main(){
+    /*variaveis*/
+    int i, j, n = 0;
+    char t;
+    double m[ORD][ORD], soma = 0;
+
+    /*entrada & processamento*/
+    scanf("%c", &t);
+    for(i = 0; i < ORD; i++){
+        for(j = 0; j < ORD; j++){
+            scanf("%lf", &m[i][j]);
+            if(i > j){
+                soma += m[i][j];
+                n++;
+            }
+        }
+    }
+
+    /*saida*/
+    if(t == 'S'){
+        printf("%.1lf\n", soma);
+    }
+    else{
+        printf("%.1lf\n", soma / (float)n);
+    }
+    return 0;
+}
