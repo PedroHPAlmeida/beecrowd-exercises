e000740eb3ed15f0189e6461d9e32cba845dfd09
Pedro Henrique Pereira Almeida <pedro.6571almeida@gmail.com>
2021-08-15 17:59:48 -0300

    Triângulos e Polígonos RegularesTriângulos e Polígonos Regulares

---
+++ 2-Ad-Hoc/ex2058.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    /*variaveis*/
+    int n;
+
+    /*entrada*/
+    scanf("%d", &n);
+
+    /*processamento & saida*/
+    printf("%d\n", n - 2);
+    return 0;
+}
