3c188e32717c835ffd36b658f29506472506e2cd
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-09-14 21:28:21 -0300

    Ex 1003 Kotlin

---
+++ 1-Iniciante/Ex1003.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val a = scanner.nextInt()
+    val b = scanner.nextInt()
+
+    val soma = a + b
+    println("SOMA = $soma")
+}
+
